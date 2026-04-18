2174c8569e17a072168532fb57dae14de4815328
Daniel Pacheco <danielpfcont@gmail.com>
2026-04-18 01:24:33 -0300

    Troca de responsabilidade do ThemeData

--- lib/core/services/theme_service.dart
+++ lib/core/services/theme_service.dart
@@ -1,14 +1,40 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:target/core/constants/style_constants.dart';
 
 abstract class ThemeService {
   static final theme = ThemeData(
     colorSchemeSeed: StyleConstants.primaryColor,
+    textTheme: GoogleFonts.dmSansTextTheme(ThemeData.light().textTheme).apply(
+      bodyColor: StyleConstants.textColor,
+      displayColor: StyleConstants.textColor,
+    ),
     scaffoldBackgroundColor: StyleConstants.screenBackgroundColor,
-    dialogTheme: DialogThemeData(backgroundColor: StyleConstants.screenBackgroundColor),
+    dialogTheme: DialogThemeData(
+      backgroundColor: StyleConstants.screenBackgroundColor,
+      shape: RoundedRectangleBorder(
+        side: BorderSide(color: StyleConstants.primaryColor),
+        borderRadius: BorderRadiusGeometry.circular(10),
+      ),
+    ),
+    appBarTheme: AppBarTheme(
+      backgroundColor: StyleConstants.screenBackgroundColor,
+      surfaceTintColor: StyleConstants.screenBackgroundColor,
+    ),
+    filledButtonTheme: FilledButtonThemeData(
+      style: FilledButton.styleFrom(
+        backgroundColor: StyleConstants.primaryColor,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadiusGeometry.circular(8),
+          side: BorderSide(color: StyleConstants.outlineBorderColor),
+        ),
+      ),
+    ),
     inputDecorationTheme: InputDecorationTheme(
       filled: true,
       fillColor: StyleConstants.fieldsBackgroundColor,
+      contentPadding: EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+      hintStyle: TextStyle(color: const Color.fromARGB(88, 0, 0, 0)),
       enabledBorder: OutlineInputBorder(
         borderRadius: BorderRadius.circular(5),
         borderSide: BorderSide(color: StyleConstants.outlineBorderColor),
@@ -16,6 +42,14 @@ abstract class ThemeService {
       focusedBorder: OutlineInputBorder(
         borderRadius: BorderRadius.circular(5),
         borderSide: BorderSide(color: StyleConstants.outlineBorderColor),
+      ),
+      errorBorder: OutlineInputBorder(
+        borderRadius: BorderRadius.circular(5),
+        borderSide: BorderSide(color: Colors.red),
+      ),
+      focusedErrorBorder: OutlineInputBorder(
+        borderRadius: BorderRadius.circular(5),
+        borderSide: BorderSide(color: Colors.red),
       ),
     ),
   );
--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
-import 'package:target/core/constants/style_constants.dart';
 import 'package:target/core/constants/keys_constants.dart';
 import 'package:target/core/services/locator_service.dart';
+import 'package:target/core/services/theme_service.dart';
 import 'package:target/core/stores/device_store.dart';
 import 'package:target/modules/login/login_screen.dart';
 
@@ -20,56 +19,7 @@ class DesafioTarget extends StatelessWidget {
       title: 'Target Sistemas Desafio',
       debugShowCheckedModeBanner: false,
       scaffoldMessengerKey: KeysConstants.snackbarKey,
-      theme: ThemeData(
-        colorSchemeSeed: StyleConstants.primaryColor,
-        textTheme: GoogleFonts.dmSansTextTheme(ThemeData.light().textTheme).apply(
-          bodyColor: StyleConstants.textColor,
-          displayColor: StyleConstants.textColor,
-        ),
-        scaffoldBackgroundColor: StyleConstants.screenBackgroundColor,
-        dialogTheme: DialogThemeData(
-          backgroundColor: StyleConstants.screenBackgroundColor,
-          shape: RoundedRectangleBorder(
-            side: BorderSide(color: StyleConstants.primaryColor),
-            borderRadius: BorderRadiusGeometry.circular(10),
-          ),
-        ),
-        appBarTheme: AppBarTheme(
-          backgroundColor: StyleConstants.screenBackgroundColor,
-          surfaceTintColor: StyleConstants.screenBackgroundColor,
-        ),
-        filledButtonTheme: FilledButtonThemeData(
-          style: FilledButton.styleFrom(
-            backgroundColor: StyleConstants.primaryColor,
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadiusGeometry.circular(8),
-              side: BorderSide(color: StyleConstants.outlineBorderColor),
-            ),
-          ),
-        ),
-        inputDecorationTheme: InputDecorationTheme(
-          filled: true,
-          fillColor: StyleConstants.fieldsBackgroundColor,
-          contentPadding: EdgeInsets.symmetric(horizontal: 10, vertical: 5),
-          hintStyle: TextStyle(color: const Color.fromARGB(88, 0, 0, 0)),
-          enabledBorder: OutlineInputBorder(
-            borderRadius: BorderRadius.circular(5),
-            borderSide: BorderSide(color: StyleConstants.outlineBorderColor),
-          ),
-          focusedBorder: OutlineInputBorder(
-            borderRadius: BorderRadius.circular(5),
-            borderSide: BorderSide(color: StyleConstants.outlineBorderColor),
-          ),
-          errorBorder: OutlineInputBorder(
-            borderRadius: BorderRadius.circular(5),
-            borderSide: BorderSide(color: Colors.red),
-          ),
-          focusedErrorBorder: OutlineInputBorder(
-            borderRadius: BorderRadius.circular(5),
-            borderSide: BorderSide(color: Colors.red),
-          ),
-        ),
-      ),
+      theme: ThemeService.theme,
       home: LayoutBuilder(
         builder: (context, constraints) {
           getIt<DeviceStore>().updateScreenWidth(constraints.maxWidth);
